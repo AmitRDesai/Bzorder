8acb0a2aba8035b712391f5026a5ab11a4cdcc3d
pujan <shahpujan1992@gmail.com>
2018-01-05 23:53:39 +0530

    home page changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TabsModule } from 'ngx-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { Ng4GeoautocompleteModule } from 'ng4-geoautocomplete';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @NgModule({
   
@@ -15,13 +17,14 @@ import { FooterComponent } from './layout/footer/footer.component';
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    TabsModule.forRoot(),
     AppRoutingModule,
-    Ng4GeoautocompleteModule.forRoot()
+    Ng4GeoautocompleteModule.forRoot(),
+    NgbDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
